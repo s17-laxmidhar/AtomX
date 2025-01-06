9a9a41da9b25c85a776a999c204de69b929ec36e
Laxmidhar Sahoo <122968002+s17-laxmidhar@users.noreply.github.com>
2025-01-05 22:45:59 -0800

    Update rate limiting
    In 15 minute, user can make 10000 request.

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(helmet());
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 10000,
     message: 'Too many requests from this IP, please try again later.',
   })
 );
